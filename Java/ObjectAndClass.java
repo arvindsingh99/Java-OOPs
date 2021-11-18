3ec8a360a3261c9cff8f1d33954fe039894580c1
arvindsingh99 <singharvi69@gmail.com>
2021-11-18 05:47:48 +0530

    P added

---
+++ Java/ObjectAndClass.java
@@ -0,0 +1,17 @@
+
+//Class Name B0x
+class ObjectAndClass {
+    int length = 20;
+
+    public void printLength() {
+
+        System.out.println("BOX Length:" + length);
+    }
+
+    public static void main(String args[]) {
+        // New object created length of class BOX
+        ObjectAndClass length = new ObjectAndClass();
+        length.printLength();
+
+    }
+}
