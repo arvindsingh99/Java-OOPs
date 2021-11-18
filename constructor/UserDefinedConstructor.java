125ed9cfb0b3df8c5b340c575f1c57040a46c11c
arvindsingh99 <singharvi69@gmail.com>
2021-11-18 05:45:37 +0530

    constructor program added

---
+++ constructor/UserDefinedConstructor.java
@@ -0,0 +1,14 @@
+class Example {
+
+    String name;
+    int id;
+    // Creating construcotr
+    Example(){
+        
+    }
+    public static void main(String args[]) {
+        Example e1 = new Example();
+        System.out.println("Default Constructor values: " + e1.name + " \n" + e1.id);
+    }
+
+}
